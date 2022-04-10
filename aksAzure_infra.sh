650a398bba24450833f495f51110b49ba6a15d27
Your Name <you@example.com>
2022-04-10 20:42:32 +0530

    tewfwef

--- aksAzure_infra.sh
+++ aksAzure_infra.sh
@@ -1,7 +1,6 @@
 #!bin/sh
 az provider register -n Microsoft.ContainerService
-az  group create  --name  aks-rg   --location eastus2
-az aks create --resource-group acr-rg --name akscluster  --location eastus  --node-count 2 --generate-ssh-keys
-az aks install-cli
-az aks get-credentials --resource-group=aks-rg --name=akscluster
+az  group create  --name  aks-rg-test   --location eastus2
+az aks create --resource-group acr-rg-test --name akscluster  --location eastus  --node-count 2 --generate-ssh-keys
+az aks get-credentials --resource-group=aks-rg-test --name=akscluster
 kubectl get nodes -o wide
