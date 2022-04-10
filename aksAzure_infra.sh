2bd4fd1c0825cdc4defd130ce566ffed6361e27b
Rajneesh <40818730+rajneeshmahala@users.noreply.github.com>
2022-04-10 17:16:31 +0530

    Update aksAzure_infra.sh

--- aksAzure_infra.sh
+++ aksAzure_infra.sh
@@ -2,6 +2,6 @@
 az provider register -n Microsoft.ContainerService
 az  group create  --name  aks-rg   --location eastus2
 az aks create --resource-group acr-rg --name akscluster  --location eastus  --node-count 2 --generate-ssh-keys
-sudo az aks install-cli
+az aks install-cli
 az aks get-credentials --resource-group=aks-rg --name=akscluster
 kubectl get nodes -o wide
